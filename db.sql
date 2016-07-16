b28670fdd9fd6878333f67d717ad0214dea2a536
blackcater <164710166@qq.com>
2016-07-16 23:15:03 +0800

    修改sql文件

--- db.sql
+++ db.sql
@@ -136,29 +136,29 @@ CREATE TABLE IF NOT EXISTS `si_user_social` (
 DROP TABLE IF EXISTS `si_user`;
 CREATE TABLE IF NOT EXISTS `si_user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL , -- 用户名
+  `username` VARCHAR(60) NOT NULL , -- 用户名
   `password` VARCHAR(32) NOT NULL , -- 密码
   -- 1 表示邮箱注册   2 表示电话注册   3 表示自定义类型账号
   `username_type` ENUM('email', 'phone', 'customer'), -- 账号类型
-  `email` VARCHAR(255) , -- 用户邮箱
+  `email` VARCHAR(60) , -- 用户邮箱
   `phone` VARCHAR(15) , -- 用户电话
   `image` TEXT , -- 用户头像
-  `signature` VARCHAR(255) , -- 用户签名
+  `signature` VARCHAR(60) , -- 用户签名
   -- 1次签到, 1积分 被赞 积分+1 分享积分+10 问答积分为别人买的数目
   `point` INT UNSIGNED NOT NULL DEFAULT 0, -- 用户签到积分
   `coin` INT UNSIGNED NOT NULL DEFAULT 0, -- 金币个数  1块钱100金币
   `user_level` INT UNSIGNED NOT NULL DEFAULT 0 , -- 用户等级
-  `school_code` INT UNSIGNED , -- 学校id
+  `school_code` SMALLINT UNSIGNED , -- 学校id
   `city_code` SMALLINT UNSIGNED , -- 所属城市
   `user_type` TINYINT UNSIGNED , -- 用户类型
   `user_social` INT UNSIGNED , -- 用户社交账号绑定信息
   `last_login_city` SMALLINT UNSIGNED , -- 上次登录城市
-  `last_login_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 上次登录时间
-  `last_register_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 上次签到时间
+  `last_login_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 上次登录时间
+  `last_register_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 上次签到时间
   `is_active` TINYINT UNSIGNED NOT NULL , -- 用户是否已经被激活
-  `active_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 用户账户被激活的时间
-  `apply_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 用户账户 发送邮箱, 短信验证的时间 有效时间为30分钟
-  `apply_code` int(10) unsigned DEFAULT 0 NOT NULL , -- 用户账户 收到的邮箱验证码或者短信验证码 md5 加密值
+  `active_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 用户账户被激活的时间
+  `apply_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 用户账户 发送邮箱, 短信验证的时间 有效时间为30分钟
+  `apply_code` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 用户账户 收到的邮箱验证码或者短信验证码 md5 加密值
   PRIMARY KEY `pk_user` (`id`) ,
   FOREIGN KEY `fk_user_school` (`school_code`) REFERENCES `si_school` (`code`) ,
   FOREIGN KEY `fk_user_city` (`city_code`) REFERENCES `si_city` (`code`) ,
@@ -181,8 +181,8 @@ CREATE TABLE IF NOT EXISTS `si_advertisement` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `url` TEXT , -- 广告图片地址
   `level` INT UNSIGNED NOT NULL , -- 广告优先级
-  `s_date` int(10) unsigned DEFAULT 0 NOT NULL, -- 投放广告时间
-  `e_date` int(10) unsigned DEFAULT 0 NOT NULL, -- 广告投放结束时间
+  `s_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL, -- 投放广告时间
+  `e_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL, -- 广告投放结束时间
   PRIMARY KEY `pk_advertisement` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -204,7 +204,7 @@ CREATE TABLE IF NOT EXISTS `si_advertisement` (
 DROP TABLE IF EXISTS `si_question`;
 CREATE TABLE IF NOT EXISTS `si_question` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `title` VARCHAR(255) NOT NULL , -- 问题标题
+  `title` VARCHAR(60) NOT NULL , -- 问题标题
   `user_id` INT UNSIGNED NOT NULL , -- 回答问题的用户id
   `url` TEXT , -- 回答的音频资源地址
   `duration` SMALLINT UNSIGNED NOT NULL DEFAULT 0 , -- 音频持续时间
@@ -215,8 +215,8 @@ CREATE TABLE IF NOT EXISTS `si_question` (
   `look` INT UNSIGNED NOT NULL DEFAULT 0 , -- 多少人听过
   `save` INT UNSIGNED NOT NULL DEFAULT 0 , -- 多少人收藏
   `praise` INT UNSIGNED NOT NULL DEFAULT 0 , -- 多少人赞过
-  `publish_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 问答发布时间
-  `last_look_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 最近一次浏览时间
+  `publish_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 问答发布时间
+  `last_look_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 最近一次浏览时间
   PRIMARY KEY `pk_question` (`id`) ,
   FOREIGN KEY `fk_question_user` (`user_id`) REFERENCES `si_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -254,8 +254,8 @@ CREATE TABLE IF NOT EXISTS `si_video` (
   `category` SMALLINT UNSIGNED NOT NULL , -- 课程所属分类
   `learn` INT UNSIGNED NOT NULL DEFAULT 0 , -- 多少人正在学习
   `save` INT UNSIGNED NOT NULL DEFAULT 0 , -- 多少人收藏
-  `publish_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 课程发布时间
-  `last_look_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 最近一次浏览时间
+  `publish_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 课程发布时间
+  `last_look_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 最近一次浏览时间
   PRIMARY KEY `pk_video` (`id`) ,
   FOREIGN KEY `fk_video_category` (`category`) REFERENCES `si_category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -269,7 +269,7 @@ CREATE TABLE IF NOT EXISTS `si_video_comment` (
   `video_id` INT UNSIGNED NOT NULL ,
   `user_id` INT UNSIGNED NOT NULL ,
   `comment` TEXT ,
-  `publish_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 评论发布时间
+  `publish_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 评论发布时间
   `praise` INT UNSIGNED NOT NULL DEFAULT 0 , -- 评论被赞数目
   `sub_id` INT UNSIGNED NOT NULL , -- 被回复的评论id
   PRIMARY KEY `pk_videoComment` (`id`) ,
@@ -286,7 +286,7 @@ DROP TABLE IF EXISTS `si_resource`;
 CREATE TABLE IF NOT EXISTS `si_resource` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `title` VARCHAR(60) NOT NULL , -- 资源标题
-  `description` TEXT NOT NULL , -- 资源详细描述
+  `description` TEXT , -- 资源详细描述
   `resource_type` VARCHAR(10) , -- 视频, PDF, 空表示不清楚
   `url` TEXT , -- 下载地址
   `user_id` INT UNSIGNED NOT NULL , -- 资源分享的人
@@ -308,7 +308,7 @@ CREATE TABLE IF NOT EXISTS `si_resource_comment` (
   `resource_id` INT UNSIGNED NOT NULL ,
   `user_id` INT UNSIGNED NOT NULL ,
   `comment` TEXT ,
-  `publish_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 评论发布时间
+  `publish_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 评论发布时间
   `praise` INT UNSIGNED NOT NULL DEFAULT 0 , -- 评论被赞数目
   `sub_id` INT UNSIGNED NOT NULL , -- 被回复的评论id
   PRIMARY KEY `pk_resourceComment` (`id`) ,
@@ -327,6 +327,7 @@ CREATE TABLE IF NOT EXISTS `si_resource_comment` (
 DROP TABLE IF EXISTS `si_activity`;
 CREATE TABLE IF NOT EXISTS `si_activity` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `user_id` INT UNSIGNED NOT NULL ,
   `title` VARCHAR(60) NOT NULL , -- 活动标题
   `address` VARCHAR(255) NOT NULL , -- 活动地点
   `image` TEXT , -- 活动宣传图
@@ -342,7 +343,8 @@ CREATE TABLE IF NOT EXISTS `si_activity` (
   `save` INT UNSIGNED NOT NULL , -- 多少个人收藏
   `look` INT UNSIGNED NOT NULL , -- 多少个人查阅
   `join` INT UNSIGNED NOT NULL , -- 多少个人参加
-  PRIMARY KEY `pk_activity` (`id`)
+  PRIMARY KEY `pk_activity` (`id`) ,
+  FOREIGN KEY `fk_activity_user` (`user_id`) REFERENCES `si_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -353,7 +355,7 @@ CREATE TABLE IF NOT EXISTS `si_activity_comment` (
   `activity_id` INT UNSIGNED NOT NULL , -- 评论的哪条资讯
   `user_id` INT UNSIGNED NOT NULL , -- 发布评论人
   `comment` TEXT , -- 评论内容
-  `publish_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 评论发布时间
+  `publish_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 评论发布时间
   `praise` INT UNSIGNED NOT NULL DEFAULT 0 , -- 评论被赞数目
   `sub_id` INT UNSIGNED NOT NULL , -- 被回复的评论id
   PRIMARY KEY `pk_activityComment` (`id`) ,
@@ -493,7 +495,7 @@ CREATE TABLE IF NOT EXISTS `si_news_comment` (
   `news_id` INT UNSIGNED NOT NULL , -- 评论的哪条资讯
   `user_id` INT UNSIGNED NOT NULL , -- 发布评论人
   `comment` TEXT , -- 评论内容
-  `publish_date` int(10) unsigned DEFAULT 0 NOT NULL , -- 评论发布时间
+  `publish_date` INT(10) UNSIGNED DEFAULT 0 NOT NULL , -- 评论发布时间
   `praise` INT UNSIGNED NOT NULL DEFAULT 0 , -- 评论被赞数目
   `sub_id` INT UNSIGNED NOT NULL , -- 被回复的评论id
   PRIMARY KEY `pk_newsComment` (`id`) ,
