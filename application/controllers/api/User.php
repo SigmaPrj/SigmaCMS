7ecfdf107a49e1c837c49a345d5c6ba527fa12bd
汤轶侬 <blackcater@blackcaterMBP.local>
2016-08-09 20:02:18 +0800

    修改注册 api

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -108,7 +108,53 @@ class User extends API_Middleware
         $type = $this->get('type');
 
         if (!isset($id)) {
-            // TODO : 上传一个用户信息
+            $user = [];
+            $user['username_type'] = $this->post('username_type');
+            $user['password'] = $this->post('password');
+
+            if ($user['username_type'] === 'email') {
+                $user['username'] = $this->post('email');
+                $user['email'] = $this->post('email');
+            } else if ($user['username_type'] === 'phone') {
+                $user['username'] = $this->post('phone');
+                $user['phone'] = $this->post('phone');
+            } else {
+                $user['username'] = $this->post('customer');
+                $user['customer'] = $this->post('customer');
+            }
+
+            $this->load->model('User_model', 'userModel');
+            if ($this->userModel->checkoutUsername($user['username'])) {
+                $user['nickname'] = '';
+                $this->load->model('UserSocial_model', 'usModel');
+                $user['user_social'] = $this->usModel->createRow();
+                $this->load->model('UserPrivilege_model', 'upModel');
+                $user['user_privilege'] = $this->upModel->createRow();
+                $user['is_active'] = 1;
+                $user['active_date'] = time();
+
+                if ($user_id = $this->userModel->addUser($user)) {
+                    $this->response([
+                        'status' => true,
+                        'code' => REST_Controller::HTTP_OK,
+                        'data' => [
+                            'user_id' => $user_id
+                        ]
+                    ], REST_Controller::HTTP_OK);
+                } else {
+                    $this->response([
+                        'status' => false,
+                        'code' => REST_Controller::HTTP_FORBIDDEN,
+                        'error' => 'Register failed!'
+                    ], REST_Controller::HTTP_FORBIDDEN);
+                }
+            } else {
+                $this->response([
+                    'status' => false,
+                    'code' => REST_Controller::HTTP_NOT_FOUND,
+                    'error' => 'Username has existed!'
+                ], REST_Controller::HTTP_NOT_FOUND);
+            }
         }
 
         if (!isset($type)) {
